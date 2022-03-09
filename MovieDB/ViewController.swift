950c652bd017830b3bb235b03ac8917cc2b659c5
scaltielgloriaa <65727457+scaltielgloriaa@users.noreply.github.com>
2022-03-09 11:22:41 +0700

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Scaltiel Gloria on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
